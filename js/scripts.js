bd16a3fde454961c4962f9e559b957faa86b780e
tluckner <161612345+tluckner@users.noreply.github.com>
2025-12-29 16:11:48 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -214,7 +214,7 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
+        if (MD5($('#invite_code').val()) !== '7409a91d5689a33a7f412422dbd6f89f'
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
