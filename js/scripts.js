f62bb6b8a456865307bf0c1a91da653321964b37
tluckner <161612345+tluckner@users.noreply.github.com>
2026-02-01 23:20:08 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -194,7 +194,7 @@ $(document).ready(function () {
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Dec, 2026 21:00'),
+            end: new Date('Dec 05, 2026 21:00'),
 
             // Event Address
             address: 'Memorial Presbyterian Church, St. Augustine, FL',
