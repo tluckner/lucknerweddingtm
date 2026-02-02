6a92a3e959603ef0f827c5626661274e8af1422e
tluckner <161612345+tluckner@users.noreply.github.com>
2026-02-01 23:16:44 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -184,23 +184,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Tommy and Maria's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Dec 05, 2026 03:15'),
 
             // Event duration (IN MINUTES)
-            // duration: 120,
+            // duration: 345,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Dec, 2026 09:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Memorial Presbyterian Church, St. Augustine, FL',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "For any questions contact Tommy, Maria, or their families."
         }
     });
 
